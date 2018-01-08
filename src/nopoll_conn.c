eb486d13ad51183d918847caa8f72580af8c1f53
mander207 <Minas_Anderiasian@cable.comcast.com>
2018-01-08 11:35:13 -0800

    Remove extra debug code I added.

--- src/nopoll_conn.c
+++ src/nopoll_conn.c
@@ -590,32 +590,26 @@ SSL_CTX * __nopoll_conn_get_ssl_context (noPollCtx * ctx, noPollConn * conn, noP
 		/* flexible method */
 		return SSL_CTX_new (is_client ? TLS_client_method () : TLS_server_method ());
 #elif defined(NOPOLL_HAVE_TLSv12_ENABLED) && OPENSSL_VERSION_NUMBER < 0x10100000L
-#error NOPOLL_HAVE_TLSv12_ENABLED
 		nopoll_log (ctx, NOPOLL_LEVEL_DEBUG, "choosing default tls-method=tlsv1.2 conn-id=%d", conn->id);
 		/* if not use TLSv1.2 */
 		return SSL_CTX_new (is_client ? TLSv1_2_client_method () : TLSv1_2_server_method ());
 #elif defined(NOPOLL_HAVE_TLSv11_ENABLED) && OPENSSL_VERSION_NUMBER < 0x10100000L
-#error NOPOLL_HAVE_TLSv11_ENABLED
 		nopoll_log (ctx, NOPOLL_LEVEL_DEBUG, "choosing default tls-method=tlsv1.1 conn-id=%d", conn->id);
 		/* if not use TLSv1.1 */
 		return SSL_CTX_new (is_client ? TLSv1_1_client_method () : TLSv1_1_server_method ());
 #elif defined(NOPOLL_HAVE_TLSv10_ENABLED) && OPENSSL_VERSION_NUMBER < 0x10100000L
-#error NOPOLL_HAVE_TLSv10_ENABLED
 		nopoll_log (ctx, NOPOLL_LEVEL_DEBUG, "choosing default tls-method=tlsv1.0 conn-id=%d", conn->id);
 		/* by default use TLSv1.0 */
 		return SSL_CTX_new (is_client ? TLSv1_client_method () : TLSv1_server_method ());
 #elif defined(NOPOLL_HAVE_SSLv3_ENABLED) && OPENSSL_VERSION_NUMBER < 0x10100000L
-#error NOPOLL_HAVE_SSLv3_ENABLED
 		nopoll_log (ctx, NOPOLL_LEVEL_DEBUG, "choosing default tls-method=sslv3 conn-id=%d", conn->id);
 		/* if not use SSLv3 */
 		return SSL_CTX_new (is_client ? SSLv3_client_method () : SSLv3_server_method ());
 #elif defined(NOPOLL_HAVE_SSLv23_ENABLED) && OPENSSL_VERSION_NUMBER < 0x10100000L
-#error NOPOLL_HAVE_SSLv23_ENABLED
 		nopoll_log (ctx, NOPOLL_LEVEL_DEBUG, "choosing default tls-method=sslv23 conn-id=%d", conn->id);
 		/* if not use SSLv23 */
 		return SSL_CTX_new (is_client ? SSLv23_client_method () : SSLv23_server_method ()); 
 #else
-#error no_default_method_found
 		/* no default method found */
 		return NULL;
 #endif
